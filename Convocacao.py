60fda6f45a5f839fa602eeca6fba8b5194c0b3ba
Cleo-Albuquerque <lopescleandson@gmail.com>
2022-12-28 22:30:47 -0200

    projeto inicial

---
+++ Convocacao.py
@@ -0,0 +1,60 @@
+nome_jogador= input()
+if (nome_jogador == 'Alisson'):
+  print(f'{nome_jogador} foi convocado e jogará como goleiro!')
+elif (nome_jogador == 'Ederson'):
+  print(f'{nome_jogador} foi convocado e jogará como goleiro!')
+elif (nome_jogador == 'Weverton'):
+  print(f'{nome_jogador} foi convocado e jogará como goleiro!')
+
+elif (nome_jogador == 'Daniel Alves'):
+  print(f'{nome_jogador} foi convocado e jogará como lateral!')
+elif (nome_jogador == 'Danilo'):
+  print(f'{nome_jogador} foi convocado e jogará como lateral!')
+elif (nome_jogador == 'Alex Sandro'):
+  print(f'{nome_jogador} foi convocado e jogará como lateral!')
+elif (nome_jogador == 'Alex Telles'):
+  print(f'{nome_jogador} foi convocado e jogará como lateral!')
+
+elif (nome_jogador == 'Marquinhos'):
+  print(f'{nome_jogador} foi convocado e jogará como zagueiro!')
+elif (nome_jogador == 'Thiago Silva'):
+  print(f'{nome_jogador} foi convocado e jogará como zagueiro!')
+elif (nome_jogador == 'Éder Militão'):
+  print(f'{nome_jogador} foi convocado e jogará como zagueiro!')
+elif (nome_jogador == 'Bremer'):
+  print(f'{nome_jogador} foi convocado e jogará como zagueiro!')
+
+elif (nome_jogador == 'Casemiro'):
+  print(f'{nome_jogador} foi convocado e jogará como meia!')
+elif (nome_jogador == 'Fabinho'):
+  print(f'{nome_jogador} foi convocado e jogará como meia!')
+elif (nome_jogador == 'Fred'):
+  print(f'{nome_jogador} foi convocado e jogará como meia!')
+elif (nome_jogador == 'Bruno Guimarães'):
+  print(f'{nome_jogador} foi convocado e jogará como meia!')
+elif (nome_jogador == 'Lucas Paquetá'):
+  print(f'{nome_jogador} foi convocado e jogará como meia!')
+elif (nome_jogador == 'Everton Ribeiro'):
+  print(f'{nome_jogador} foi convocado e jogará como meia!')
+
+elif (nome_jogador == 'Neymar'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Raphinha'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Vini Jr.'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Antony'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Richarlison'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Rodrygo'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Pedro'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Gabriel Jesus'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+elif (nome_jogador == 'Gabriel Martinelli'):
+  print(f'{nome_jogador} foi convocado e jogará como atacante!')
+
+else:
+  print(f'{nome_jogador} não foi convocado, mas quem sabe na próxima?')
